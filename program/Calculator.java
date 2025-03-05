f7cddd27420a37004ea004f9ecd3d7d082099eaa
smrive4 <193434163+smrive4@users.noreply.github.com>
2025-03-05 17:16:24 -0600

    Add Changes to Main Driver

---
+++ program/Calculator.java
@@ -0,0 +1,25 @@
+/**
+ * Calculator that calculates various formulas
+ * 
+ * @author Gavin Garcia
+ * @author Daniel Golladay
+ * @author Carl Ghess
+ * @author Saul Rivera 
+ */
+
+public class Calculator{
+    /**
+     * Main Driver
+     */
+    public static void main(String[] args) {
+        
+    }
+
+    /**
+     * Display Menu Options
+     */
+    public static void displayMenu(){
+
+    }
+
+}
