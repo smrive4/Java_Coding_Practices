15add523fdd0a4934bbc8821857646c2d9a533a5
Carl Ghess <carl.ghess@gmail.com>
2025-03-06 17:29:49 -0600

    Implemented Circle and Rectangle classes. added serialization to MyMath

--- program/MyMath.java
+++ program/MyMath.java
@@ -1,5 +1,32 @@
-public abstract class MyMath {
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
-  abstract double solve();
+public abstract class MyMath implements Serializable{
+
   abstract String getHistory();
+
+  /**
+   * Writes the state of the object into a stream. Uses Java's built in serialization
+   * 
+   * @param stream the stream the object will be written to
+   * @throws IOException
+   */
+  private void writeObject(final ObjectOutputStream stream)
+      throws IOException {
+    stream.defaultWriteObject();
+  }
+
+  /**
+   * Reads the state of the object from an input stream. Uses Java's built in serialization
+   * 
+   * @param stream the stream the object will be read from
+   * @throws IOException
+   * @throws ClassNotFoundException
+   */
+  private void readObject(final ObjectInputStream stream)
+      throws IOException, ClassNotFoundException {
+    stream.defaultReadObject();
+  }
 }
