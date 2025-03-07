a3d93ddd793f07ec9b16821a15495915fcd1cb2a
smrive4 <193434163+smrive4@users.noreply.github.com>
2025-03-06 18:48:24 -0600

    Add Javadoc comments

--- program/Circle.java
+++ program/Circle.java
@@ -1,17 +1,38 @@
-public class Circle extends TwoDimensions {
 
-  double radius;
+/**
+ * Circle Class
+ */
+
+public class Circle extends TwoDimensions {
+  /** radius of a circle*/
+  private double radius;
+
+  /**
+   * Constructor
+   * 
+   * @param radius radius of a circle
+   */
   public Circle(double radius) {
     this.radius = radius;
   }
 
+  /**
+   * Method to get area
+   * 
+   * @return area of a circle
+   */
   @Override
-  double getArea() {
+  public double getArea() {
     return Math.PI * (Math.pow(radius, 2));
   }
 
+  /**
+   * Method to return information of a Circle object
+   * 
+   * @return a string containing the radius and area of a circle
+   */
   @Override
-  String getHistory() {
+  public String getHistory() {
     return "Area of circle calculated:\nradius: " + radius + "\nresult: " + getArea();
   }
 }
