82c79fce7e2d6426c3e60ca11b42139803824592
smrive4 <193434163+smrive4@users.noreply.github.com>
2025-03-06 19:15:56 -0600

    Add Javadoc comments

--- program/TwoDimensions.java
+++ program/TwoDimensions.java
@@ -1,4 +1,14 @@
+
+/**
+ * Abstract Class for shapes with two dimensions
+ */
+
 public abstract class TwoDimensions extends MyMath {
 
+  /**
+   * Abstract Method to get area
+   * 
+   * @return area of a 2d shape 
+   */
   abstract double getArea();
 }
